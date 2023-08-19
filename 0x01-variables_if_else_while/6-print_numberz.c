92565b7f1a772bbd6767c1de4f931b0f53850085
Kaddijatou Jallow <dijadiju97@gmail.com>
2023-08-19 05:05:38 -0700

    prints single digits

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - Entry point 
+ * main - Entry point
  *
  * Return: Always 0(Success)
  */
 int main(void)
 {
-	char ch;
+	int  x;
 
-	for (ch = '0'; ch <= '9'; ch++)
-		putchar(ch);
+	for (x = 0; x < 10; x++)
+		putchar(x + '0');
+
 	putchar('\n');
 	return (0);
 }
