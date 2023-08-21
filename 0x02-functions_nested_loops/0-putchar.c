bc6c11d89b991d0ad9633787502c14c130b63484
Kaddijatou Jallow <dijadiju97@gmail.com>
2023-08-21 11:50:35 -0700

    print _putchar to stdout

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,14 +2,20 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes _putchar to stdout
+ * main - Entry point of my code
  *
  * Return: Always 0(Success)
  */
-int _putchar(void)
+int main(void)
 {
-	char *str = "_putchar\n";
-	ssize_t len = 9;
-
-	return (write(1, str, len));
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
